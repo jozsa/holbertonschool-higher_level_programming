b3de333cd04239f61a3a2c39b34724bab0cc7643
jozsa <allisonjoyweiner@gmail.com>
2018-12-04 23:02:34 +0000

    Changing multiplication line

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 print('Last digit of {:d} is'.format(number), end=" ")
 lastdigit = abs(number) % 10
 if number < 0:
-    lastdigit = lastdigit * -1
+    lastdigit = -lastdigit
 if lastdigit == 0:
     print('{:d} and is 0'.format(lastdigit))
 elif lastdigit < 6 and lastdigit != 0:
