36485a9b665b8742c4cbb5666e8430a49a5f074c
jozsa <allisonjoyweiner@gmail.com>
2018-12-05 04:28:47 +0000

    Changing the printing statements

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print('Last digit of {:d} is '.format(number), end="")
 lastdigit = abs(number) % 10
+print('Last digit of {:d} is {:d} '.format(number, lastdigit), end="")
 if number < 0:
     lastdigit = -lastdigit
 if lastdigit == 0:
-    print('{:d} and is 0'.format(lastdigit))
+    print('and is 0')
 elif lastdigit < 6 and lastdigit != 0:
-    print('{:d} and is less than 6 and not 0'.format(lastdigit))
+    print('and is less than 6 and not 0')
 elif lastdigit > 5:
-    print('{:d} and is greater than 5'.format(lastdigit))
+    print('and is greater than 5')
