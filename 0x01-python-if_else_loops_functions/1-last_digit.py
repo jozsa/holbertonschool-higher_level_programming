6f0f2c7594f94f7a5270a3b7bc4bd395d0462694
jozsa <allisonjoyweiner@gmail.com>
2018-12-05 04:31:45 +0000

    Edited conditions to check negative numbers correctly

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 lastdigit = abs(number) % 10
-print('Last digit of {:d} is {:d} '.format(number, lastdigit), end="")
 if number < 0:
     lastdigit = -lastdigit
+print('Last digit of {:d} is {:d} '.format(number, lastdigit), end="")
 if lastdigit == 0:
     print('and is 0')
-elif lastdigit < 6 and lastdigit != 0:
+elif lastdigit < 6:
     print('and is less than 6 and not 0')
 elif lastdigit > 5:
     print('and is greater than 5')
