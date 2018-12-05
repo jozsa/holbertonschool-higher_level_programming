6e415700615743f22e7b25ed0497a291f3fc64ff
jozsa <allisonjoyweiner@gmail.com>
2018-12-05 19:59:19 +0000

    Moved total variable declaration into import exclusion condition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from sys import argv
-total = 0
 if __name__ == "__main__":
+    total = 0
     for i, name in enumerate(argv[1:]):
         total += int(name)
     print('{}'.format(total))
