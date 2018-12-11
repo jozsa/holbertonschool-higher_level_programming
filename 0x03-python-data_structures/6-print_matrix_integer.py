0600e17e2df83b09ee80300a0049f056c367bd3f
jozsa <allisonjoyweiner@gmail.com>
2018-12-11 22:13:59 +0000

    Printing a matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    size = len(matrix[0])
+    for row in range(len(matrix)):
+        for column in range(size):
+            if column < size - 1:
+                sep = ' '
+            else:
+                sep = ''
+            print('{:d}'.format(matrix[row][column]), end=sep)
+        print()
+    
