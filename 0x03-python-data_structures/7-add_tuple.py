26d589bce28803fa82dd854c5bd71e84a8b60f36
jozsa <allisonjoyweiner@gmail.com>
2018-12-11 23:24:23 +0000

    Change return value to return tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,4 +8,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tuple_b = tuple_b + (0,)
     total_a = tuple_a[0] + tuple_b[0]
     total_b = tuple_a[1] + tuple_b[1]
-    return total_a, total_b
+    total_tuple = (total_a, total_b)
+    return total_tuple
