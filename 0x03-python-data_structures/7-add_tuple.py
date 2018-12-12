0b6e3aea7fea99aff44e8af5dd5c508d7ab3aab5
jozsa <allisonjoyweiner@gmail.com>
2018-12-12 07:04:22 +0000

    Recreated the tuples after reassignment

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,6 +6,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
         for i in range(2):
             tuple_a = tuple_a + (0,)
             tuple_b = tuple_b + (0,)
+    tuple_a = (tuple_a[0], tuple_a[1])
+    tuple_b = (tuple_b[0], tuple_b[1])
     total_a = tuple_a[0] + tuple_b[0]
     total_b = tuple_a[1] + tuple_b[1]
     total_tuple = (total_a, total_b)
