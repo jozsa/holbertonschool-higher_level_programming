72a21ebafe17997f4e846ee7c1e16d2bf068d016
jozsa <allisonjoyweiner@gmail.com>
2018-12-12 06:58:57 +0000

    Removed unnecessary lines

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if type(tuple_a) is not tuple:
-        tuple_a = (0, 0)
     len_a = len(tuple_a)
-    if type(tuple_b) is not tuple:
-        tuple_b = (0, 0)
     len_b = len(tuple_b)
-    tuple_a = tuple(x if x else '0' for x in tuple_a)
-    tuple_b = tuple(x if x else '0' for x in tuple_b)
     if len_a or len_b < 2:
         for i in range(2):
             tuple_a = tuple_a + (0,)
