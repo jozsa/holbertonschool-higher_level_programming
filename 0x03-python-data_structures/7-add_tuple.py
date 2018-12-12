e956f144191d3c6c8c0b622bf56d3ef24ea87393
jozsa <allisonjoyweiner@gmail.com>
2018-12-12 00:25:58 +0000

    Change test for tuple type

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if type(tuple_a) and type(tuple_b) is tuple:
-        len_a = len(tuple_a)
-        len_b = len(tuple_b)
-        tuple_a = tuple(x if x else '0' for x in tuple_a)
-        tuple_b = tuple(x if x else '0' for x in tuple_b)
-        if len_a or len_b < 2:
-            for i in range(2):
-                tuple_a = tuple_a + (0,)
-                tuple_b = tuple_b + (0,)
-        total_a = tuple_a[0] + tuple_b[0]
-        total_b = tuple_a[1] + tuple_b[1]
-        total_tuple = (total_a, total_b)
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
+    if type(tuple_a) is not tuple:
+        tuple_a = (0, 0)
+    if type(tuple_b) is not tuple:
+        tuple_b = (0, 0)
+    tuple_a = tuple(x if x else '0' for x in tuple_a)
+    tuple_b = tuple(x if x else '0' for x in tuple_b)
+    if len_a or len_b < 2:
+        for i in range(2):
+            tuple_a = tuple_a + (0,)
+            tuple_b = tuple_b + (0,)
+    total_a = tuple_a[0] + tuple_b[0]
+    total_b = tuple_a[1] + tuple_b[1]
+    total_tuple = (total_a, total_b)
     return total_tuple
