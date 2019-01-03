6807da0575ac4c7b9cd7459e94a2a6b2167d50b4
jozsa <allisonjoyweiner@gmail.com>
2019-01-03 00:38:06 +0000

    Deleted main file and added safe print list with non-sliced indices

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     length = 0
-    for element in my_list[:x]:
+    for i in range(x):
         try:
-            print('{:d}'.format(element), end='')
+            print('{:d}'.format(my_list[i]), end='')
             length += 1
         except (TypeError, ValueError):
             pass
