e9f535949628698c2fdf95104ff7bf45e39ad7a9
jozsa <allisonjoyweiner@gmail.com>
2019-01-03 00:01:12 +0000

    PEP8 Compliance

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     length = 0
     try:
@@ -7,5 +6,5 @@ def safe_print_list(my_list=[], x=0):
             length += 1
         print(''.join(str(i) for i in my_list[:x]))
     except Exception:
-        print('Could not print list') 
+        print('Could not print list')
     return length
