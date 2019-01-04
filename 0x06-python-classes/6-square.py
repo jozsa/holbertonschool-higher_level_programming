99b0933a2862b31a0310c716b72d56ec5a397506
jozsa <allisonjoyweiner@gmail.com>
2019-01-04 01:21:30 +0000

    Edited error messages some more

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -56,8 +56,9 @@ class Square:
         """Prints a hashtag square using the size of the square"""
         if self.__size == 0:
             print('')
-        for i in range(self.__position[1]):
-            print('')
+        if (self.__position[1] > 0):
+            for i in range(self.__position[1]):
+                print('')
         for i in range(self.__size):
             print(' ' * self.__position[0], end='')
             print('#' * self.__size)
