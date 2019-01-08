613f4593f3eacf8b6359118db989257d7c2c356a
jozsa <allisonjoyweiner@gmail.com>
2019-01-08 05:06:13 +0000

    PEP8 compliance and fixed one missed test

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,13 +5,16 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError('division by zero')
     if not isinstance(matrix, list):
-        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+        raise TypeError('matrix must be a matrix ' \
+                        '(list of lists) of integers/floats')
+    if not all(isinstance(rows, list) for rows in matrix):
+        raise TypeError('matrix must be a matrix ' \
+                        '(list of lists) of integers/floats')
     if not all(len(rows) == len(matrix[0]) for rows in matrix):
         raise TypeError('Each row of the matrix must have the same size')
-    if not all(isinstance(rows, list) for rows in matrix):
-        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
     for rows in matrix:
         if not all(isinstance(cols, (float, int)) for cols in rows):
-            raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+            raise TypeError('matrix must be a matrix ' \
+                            '(list of lists) of integers/floats')
     newmatrix = [[round(cols/div, 2) for cols in rows] for rows in matrix]
     return newmatrix
