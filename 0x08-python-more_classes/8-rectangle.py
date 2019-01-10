29a9115b31f1b8aaa0ec7e18d76ef667a95e9549
jozsa <allisonjoyweiner@gmail.com>
2019-01-10 02:24:48 +0000

    Returning rectangles not their area

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -89,8 +89,7 @@ class Rectangle:
             raise TypeError('rect_1 must be an instance of Rectangle')
         if not isinstance(rect_2, Rectangle):
             raise TypeError('rect_2 must be an instance of Rectangle')
-        if rect_1.area() == rect_2.area():
-            bigger = rect_1
+        if rect_1.area() >= rect_2.area():
+            return rect_1
         else:
-            bigger = max(rect_1.area(), rect_2.area())
-        return bigger
+            return rect_2
