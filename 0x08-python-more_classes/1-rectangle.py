7e3590fba6343a35fe892c6b3fb2ff396491583f
jozsa <allisonjoyweiner@gmail.com>
2019-01-10 00:18:39 +0000

    Instantation with height and width

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+class Rectangle:
+    """Rectangle instantiated with width and height
+
+    Attributes:
+        width (int, optional): width to instantiate rectangle with
+        height (int, optional): height to instantiate rectangle with
+    """
+    def __init__(self, width=0, height=0):
+        """Instantation of Rectangle class with
+        optional height and width"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Property getter of private attribute width
+
+        Property setter: width(self, value) sets the size of the Rectangle
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    """Property getter of private attribute height
+
+    Property setter: height(self, value) sets the size of the Rectangle
+    """
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
