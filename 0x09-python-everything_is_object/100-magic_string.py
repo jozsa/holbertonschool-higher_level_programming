b756c20008bd7060dc6851c10d3942adfdff4f5d
jozsa <allisonjoyweiner@gmail.com>
2019-01-11 22:23:48 +0000

    Printing a string magically, for real this time

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    hbtn = "Holberton, " * i
-    return hbtn.rstrip(', ')
+def magic_string(hbtn=[]):
+    hbtn.insert(0, "Holberton, ")
+    return ''.join(hbtn).rstrip(', ')
