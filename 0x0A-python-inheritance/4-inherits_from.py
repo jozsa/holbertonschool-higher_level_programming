5b794385e72c6b2b1b50ea43b277202c1fc776a6
jozsa <allisonjoyweiner@gmail.com>
2019-01-16 23:45:00 +0000

    Testing whether an object is a subclass of another class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""inherits_from(obj, a_class)
+
+Tests whether an object is an
+instance inherited from a class
+
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns true if obj is an instance
+    inherited from a_class. Otherwise,
+    False is returned.
+    """
+    if not type(obj) is a_class and issubclass(type(obj), a_class):
+        return True
