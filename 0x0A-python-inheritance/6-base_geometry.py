27fdd988db472da22acc763512fb12546a9a49e4
jozsa <allisonjoyweiner@gmail.com>
2019-01-16 23:51:19 +0000

    Writing a class with an unimplemented method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """
+    BaseGeometry class with non-implemented area() method
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
