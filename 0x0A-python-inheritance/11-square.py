bf9e4f33b8a1b8adef7bd3863869bb1ed36bb8ed
jozsa <allisonjoyweiner@gmail.com>
2019-01-17 18:44:00 +0000

    Uploading right file

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square subclass of Rectangle instantiated with size
+
+    Attributes:
+        size (int, required): size to instantiate square with
+    """
+    def __init__(self, size):
+        """Instantation of Square class instance with
+        required size. Instantation is inherited from
+        Rectangle class with the addition of size"""
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """Redefines str() and print() to print a visual
+        representation of the square with the length of 
+        its sides."""
+        return '[Square] {}/{}'.format(self.__size, self.__size)
