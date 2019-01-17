acbf5c41eb6e574b4cdd8c784b0020aacc94b216
jozsa <allisonjoyweiner@gmail.com>
2019-01-17 20:40:30 +0000

    Documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-"""
+"""read_file(filename="")
+
+This module has one function that
+reads a file and prints it to standard
+output.
 
 """
 
 
 def read_file(filename=""):
+    """
+    Prints a file to standard output
+    """
     with open(filename, encoding='utf-8') as a_file:
         print(a_file.read().rstrip())
