afa4a4ead83c3f0080343029603a3b37e5c07d20
jozsa <allisonjoyweiner@gmail.com>
2019-01-17 02:10:54 +0000

    Adding Square subclass of Rectangle which is a subclass of BaseGeometry

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square subclass of Rectangle instantiated with size
+
+    Attributes:
+        size (int, required): size to instantiate square with
+    """
+    def __init__(self, size):
+        """Instantation of Square class instance with
+        required size. Instantation is inherited from
+        Rectangle class with the addition of size"""
+        super().__init__(size, size)
+        self.__size = size
