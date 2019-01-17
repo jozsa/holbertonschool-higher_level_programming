870a0d356299f0cd8b9b41d154bc91e53f72084b
jozsa <allisonjoyweiner@gmail.com>
2019-01-17 00:31:34 +0000

    Validating value input and tests

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """
+    BaseGeometry class with non-implemented area() method
+    """
+    def area(self):
+        """Unimplemented method"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Checks whether or not value is an integer more than 0"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
