67e19f928c2e609c502194a0c3e8b3e2bc312628
jozsa <allisonjoyweiner@gmail.com>
2019-01-17 21:23:19 +0000

    Reading and printing a file up to n lines

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""read_lines(filename="", nb_lines=0)
+
+Reads n lines of a text file and prints
+it to stdout.
+"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """
+    Reads a file and prints it up to nb_lines.
+    If nb_lines is less than or equal to zero, or
+    if nb_lines is more than or equal to the number
+    of lines in the file, the whole file is printed.
+    """
+    line_count = 0
+    with open(filename, encoding='utf-8') as a_file:
+        for line in a_file:
+            if line_count < nb_lines:
+                print(line.rstrip())
+            line_count += 1
+        if nb_lines >= line_count or nb_lines <= 0:
+            a_file.seek(0)
+            print(a_file.read().rstrip())
