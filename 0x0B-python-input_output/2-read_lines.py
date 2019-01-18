50f34b6f92082302a8a509ba4ba3029b1560b8f6
jozsa <allisonjoyweiner@gmail.com>
2019-01-18 03:50:56 +0000

    Edited to account for all edge cases

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -13,12 +13,10 @@ def read_lines(filename="", nb_lines=0):
     if nb_lines is more than or equal to the number
     of lines in the file, the whole file is printed.
     """
-    line_count = 0
     with open(filename, encoding='utf-8') as a_file:
-        for line in a_file:
-            if line_count < nb_lines:
-                print(line, end='')
-            line_count += 1
+        line_count = len(list(a_file))
         if nb_lines >= line_count or nb_lines <= 0:
-            a_file.seek(0)
-            print(a_file.read(), end='')
+            nb_lines = line_count
+        a_file.seek(0)
+        for line in range(nb_lines):
+            print(a_file.readline(), end='')
