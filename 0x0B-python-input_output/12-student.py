c0e65ba4606249ef6c7501d296a4475785cebd6f
jozsa <allisonjoyweiner@gmail.com>
2019-01-18 03:00:08 +0000

    Returning a dict with a filter

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import json
+
+
+class Student:
+    """Student instantiated with first_name, last_name, and age
+
+    Attributes:
+        first_name (str, required): first name of student
+        last_name (str, required): last name of student
+        age (int, required): age of student
+    """
+    def __init__(self, first_name, last_name, age):
+        """Instantation of Student class with
+        required first_name, last_name, and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns the dictionary version of self
+        Optional argument attrs set to None by default
+        """
+        if type(attrs) is list:
+            if all(type(i) is str for i in attrs):
+                return {key:value for key, value in self.__dict__.items() if key in attrs}
+        return self.__dict__
