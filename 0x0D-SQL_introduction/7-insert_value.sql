179b000e08593c20086b6db162775826c3acc29a
jozsa <allisonjoyweiner@gmail.com>
2019-03-05 01:34:47 +0000

    Change 98 to 89

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- Insert new row in first_table in database passed into mysql command
-INSERT INTO `first_table` (`id`,`name`) VALUES ('98', "Holberton School");
+INSERT INTO `first_table` (`id`,`name`) VALUES ('89', "Holberton School");
