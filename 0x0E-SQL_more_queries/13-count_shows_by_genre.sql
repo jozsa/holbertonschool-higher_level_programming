086fc8d4d73a4449b49d9cb9fb35e5a921d8debc
jozsa <allisonjoyweiner@gmail.com>
2019-03-06 02:52:24 +0000

    Count shows by genre

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- List all genres from database passed into mysql command & count of shows in each genre
+SELECT name as genre, count(genre_id) as number_of_shows 
+FROM tv_genres RIGHT JOIN tv_show_genres
+	ON (tv_genres.id = tv_show_genres.genre_id)
+GROUP BY name
+ORDER BY number_of_shows DESC;
