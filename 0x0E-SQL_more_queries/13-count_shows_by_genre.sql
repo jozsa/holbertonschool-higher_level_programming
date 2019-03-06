a5d665aa207e7dd6f6a3cfa4534f1b62353cf6f4
jozsa <allisonjoyweiner@gmail.com>
2019-03-06 02:53:15 +0000

    Capitalize all SQL keywords

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- List all genres from database passed into mysql command & count of shows in each genre
-SELECT name as genre, count(genre_id) as number_of_shows 
+SELECT name AS genre, count(genre_id) AS number_of_shows 
 FROM tv_genres RIGHT JOIN tv_show_genres
 	ON (tv_genres.id = tv_show_genres.genre_id)
 GROUP BY name
