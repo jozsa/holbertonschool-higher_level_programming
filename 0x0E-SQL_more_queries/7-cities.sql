986d83082e1ce8ccbc8e0b70508422ebdbe3f70a
jozsa <allisonjoyweiner@gmail.com>
2019-03-06 00:58:39 +0000

    USE hbtn_0d_usa instead of create hbtn_0d_usa table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,12 +1,15 @@
 -- Create htbn_0d_usa database and cities table if they don't exist.
 -- state_id will be FOREIGN KEY that references to states table id
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities
 (
-	id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	id INT UNIQUE NOT NULL AUTO_INCREMENT,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
-	INDEX (state_id),
+	
+	PRIMARY KEY (id),
+	
 	FOREIGN KEY(state_id)
 		REFERENCES states(id)
 );
