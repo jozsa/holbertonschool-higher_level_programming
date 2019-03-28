ca03ed39d7b52bfed0e975419792f5443dd72d0b
jozsa <allisonjoyweiner@gmail.com>
2019-03-28 20:54:24 +0000

    List all cities in database, join two databases

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Connect to MySQL database using MySQLdb & list all rows in passed database
+
+Takes three arguments: MySQL username, MySQL password, database name
+"""
+
+
+import sys
+import MySQLdb
+if __name__ == "__main__":
+    cities = MySQLdb.connect(host="127.0.0.1",
+                             port=3306,
+                             user=sys.argv[1],
+                             passwd=sys.argv[2],
+                             db=sys.argv[3])
+    cur = cities.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name \
+                FROM cities \
+                JOIN states ON (cities.state_id = states.id)")
+    for row in cur.fetchall():
+        print("{}".format(row))
+    cur.close()
+    cities.close()
