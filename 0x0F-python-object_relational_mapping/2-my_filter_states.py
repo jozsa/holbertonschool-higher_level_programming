5268b23f870ee623938f40921e32c8b8849bd6b0
jozsa <allisonjoyweiner@gmail.com>
2019-03-28 20:41:11 +0000

    List all entries that match 4th argument passed to script

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Connect to MySQL database using MySQLdb & list
+all states that match the 4th argument passed
+
+Takes four arguments: MySQL username, MySQL password, database name, state name
+"""
+
+
+import sys
+import MySQLdb
+if __name__ == "__main__":
+    states = MySQLdb.connect(host="127.0.0.1",
+                             port=3306,
+                             user=sys.argv[1],
+                             passwd=sys.argv[2],
+                             db=sys.argv[3])
+    cur = states.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '%s'" % (sys.argv[4],))
+    for row in cur.fetchall():
+        print("{}".format(row))
+    cur.close()
+    states.close()
