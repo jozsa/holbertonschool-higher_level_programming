a3b02b666537173b50c08ccbb1beaa6a5dff9ce0
jozsa <allisonjoyweiner@gmail.com>
2019-03-28 21:23:35 +0000

    Change cities to usa_db as name of database

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,16 +9,16 @@ Takes three arguments: MySQL username, MySQL password, database name
 import sys
 import MySQLdb
 if __name__ == "__main__":
-    cities = MySQLdb.connect(host="127.0.0.1",
+    usa_db = MySQLdb.connect(host="127.0.0.1",
                              port=3306,
                              user=sys.argv[1],
                              passwd=sys.argv[2],
                              db=sys.argv[3])
-    cur = cities.cursor()
+    cur = usa_db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name \
                 FROM cities \
                 JOIN states ON (cities.state_id = states.id)")
     for row in cur.fetchall():
         print("{}".format(row))
     cur.close()
-    cities.close()
+    usa_db.close()
