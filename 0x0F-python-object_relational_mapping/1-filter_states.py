ef8fb1f43c0d832fd8941748b0ae8899a0fdb3b7
jozsa <allisonjoyweiner@gmail.com>
2019-03-28 19:45:49 +0000

    MySQLdb connection, print all states that start with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Connect to MySQL database using MySQLdb & list all states that start with N
+Takes three arguments: MySQL username, MySQL password, database name
+"""
+
+
+import sys
+import MySQLdb
+if __name__ == "__main__":
+    states = MySQLdb.connect(host="127.0.0.1",
+                             port=3306,
+                             user=sys.argv[1],
+                             passwd=sys.argv[2],
+                             db=sys.argv[3])
+    cur = states.cursor()
+    cur.execute("SELECT * from states WHERE name LIKE 'N%'")
+    for row in cur.fetchall():
+        print("{}".format(row))
