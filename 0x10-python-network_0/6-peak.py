61fc9b4725f8d6014f63f0a9bd88492e6a9b2fa6
jozsa <allisonjoyweiner@gmail.com>
2019-04-10 22:04:28 +0000

    Add condition for if 1st element is bigger than 2nd, or if final element is bigger than 2nd-final element

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,8 +13,12 @@ def find_peak(list_of_integers):
     if not list_of_integers:
         return None
     length = len(list_of_integers)
-    if length == 1:
+    if length == 1 or length == 2:
         return list_of_integers[0]
+    if list_of_integers[0] > list_of_integers[1]:
+        return list_of_integers[0]
+    if list_of_integers[-1] > list_of_integers[-2]:
+        return list_of_integers[-1]
     if length % 2:
         middle = int(length / 2)
     else:
