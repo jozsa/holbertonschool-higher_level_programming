5d99bf8917eee8a5a443664c8dff47120826bda8
jozsa <allisonjoyweiner@gmail.com>
2019-04-10 21:56:57 +0000

    Add >= and <= instead of > and <

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -15,18 +15,16 @@ def find_peak(list_of_integers):
     length = len(list_of_integers)
     if length == 1:
         return list_of_integers[0]
-    if list_of_integers[0] > list_of_integers[1]:
-        return list_of_integers[0]
     if length % 2:
         middle = int(length / 2)
     else:
         middle = int((length / 2) - 1)
-    if list_of_integers[middle] > list_of_integers[middle + 1] \
-            and list_of_integers[middle] > list_of_integers[middle - 1]:
+    if list_of_integers[middle] >= list_of_integers[middle + 1] \
+            and list_of_integers[middle] >= list_of_integers[middle - 1]:
         return list_of_integers[middle]
     left = list_of_integers[:middle]
     right = list_of_integers[middle:]
-    if list_of_integers[middle + 1] > list_of_integers[middle]:
+    if list_of_integers[middle + 1] >= list_of_integers[middle]:
         return find_peak(right)
     else:
         return find_peak(left)
