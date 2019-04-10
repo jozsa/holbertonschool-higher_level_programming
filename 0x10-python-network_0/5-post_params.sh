1785a39e91c768d0f3dc9ff5cb5240a22d6814d5
jozsa <allisonjoyweiner@gmail.com>
2019-04-10 20:52:08 +0000

    Separate -F for each parameter

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send POST request with email and subject, display body of response
-curl -s -X POST -F "email=hr@holbertonschool.com subject=I will always be here for PLD" "$1"
+curl -s -X POST -F "email=hr@holbertonschool.com" -F "subject=I will always be here for PLD" "$1"
