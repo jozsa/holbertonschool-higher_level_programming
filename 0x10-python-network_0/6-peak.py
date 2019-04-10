2a6eef517a576012d9aa48da4d0f4f3fc3886a7d
jozsa <allisonjoyweiner@gmail.com>
2019-04-10 22:00:24 +0000

    Add case for if array has 2 elements

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,14 +13,14 @@ def find_peak(list_of_integers):
     if not list_of_integers:
         return None
     length = len(list_of_integers)
-    if length == 1:
+    if length == 1 or length == 2:
         return list_of_integers[0]
     if length % 2:
         middle = int(length / 2)
     else:
         middle = int((length / 2) - 1)
-    if list_of_integers[middle] >= list_of_integers[middle + 1] \
-            and list_of_integers[middle] >= list_of_integers[middle - 1]:
+    if list_of_integers[middle] > list_of_integers[middle + 1] \
+            and list_of_integers[middle] > list_of_integers[middle - 1]:
         return list_of_integers[middle]
     left = list_of_integers[:middle]
     right = list_of_integers[middle:]
