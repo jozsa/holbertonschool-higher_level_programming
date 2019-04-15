93792d248f151d0c533f4cbab553f1f449c1ccd6
jozsa <allisonjoyweiner@gmail.com>
2019-04-15 23:41:10 +0000

    PEP8 compliance

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -11,6 +11,6 @@ if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     url = 'https://api.github.com/users/{}'.format(sys.argv[1])
-    r = requests.get(url, auth=(username,password))
+    r = requests.get(url, auth=(username, password))
     r = dict(r.json())
     print(r.get('id'))
