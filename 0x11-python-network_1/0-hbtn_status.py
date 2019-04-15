eafa201a9cc772202d29791c3b694ddf5abddc88
jozsa <allisonjoyweiner@gmail.com>
2019-04-15 20:23:51 +0000

    Change utf-8 to utf8

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,5 @@ import urllib.request
 if __name__ == '__main__':
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as hbtn:
         status = hbtn.read()
-    print("Body response:")
-    print("\t- type: {}\n\t- content: {}\n\t- utf-8 content: {}"
+    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
           .format(type(status), status, status.decode('utf-8')))
