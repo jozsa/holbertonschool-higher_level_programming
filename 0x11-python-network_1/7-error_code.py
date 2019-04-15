904b11ab0dd9a454359f2ac050a14134e8294122
jozsa <allisonjoyweiner@gmail.com>
2019-04-15 22:39:33 +0000

    Printing error codes using requests module

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Takes a URL, sends a request to the URL,
+and displays the body of the response.
+If there is a HTTP status code >= 400,
+print it.
+"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    r = requests.post(sys.argv[1])
+    if r.status_code >= 400:
+        print('Error code: {}'.format(r.status_code))
+    else:
+        print(r.text)
