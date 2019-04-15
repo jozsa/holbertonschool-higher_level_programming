2577aecbb3ebbe245664c895b916e0f433d3267c
jozsa <allisonjoyweiner@gmail.com>
2019-04-15 21:23:46 +0000

    Change order of arguments

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,8 +10,9 @@ import sys
 
 
 if __name__ == '__main__':
+    req = urllib.request.Request(sys.argv[1])
     try:
-        with urllib.request.urlopen(sys.argv[1]) as response:
+        with urllib.request.urlopen(req) as response:
             print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
         print('Error code: {}'.format(e.code))
