d1ee573df18bbe245eca1c248f0a5ccb937432ca
jozsa <allisonjoyweiner@gmail.com>
2019-05-07 23:33:21 +0000

    Added printer for if command line argument cannot be converted to an integer

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
 if (process.argv.length === 2) {
   console.log('Missing size');
-} else {
+} 
+else if (Math.floor(process.argv[2])) {
   for (let i = 0; i < process.argv[2]; i++) {
     console.log('X'.repeat(process.argv[2]));
   }
+}
+else {
+	console.log('Missing size');
 }
