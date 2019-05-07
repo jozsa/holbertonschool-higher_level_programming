6134c2b8051ab4a7c2fa556f20a1b8e553676bc2
jozsa <allisonjoyweiner@gmail.com>
2019-05-07 23:34:01 +0000

    Semistandard compliance

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 if (process.argv.length === 2) {
   console.log('Missing size');
-} 
-else if (Math.floor(process.argv[2])) {
+} else if (Math.floor(process.argv[2])) {
   for (let i = 0; i < process.argv[2]; i++) {
     console.log('X'.repeat(process.argv[2]));
   }
-}
-else {
-	console.log('Missing size');
+} else {
+  console.log('Missing size');
 }
