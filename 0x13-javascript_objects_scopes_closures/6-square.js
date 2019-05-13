ba785a1287983c62d984628b0f846056527d9a60
jozsa <allisonjoyweiner@gmail.com>
2019-05-13 22:17:39 +0000

    Formatting

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,7 @@
 #!/usr/bin/node
-'use strict';
 const mySquare = require('./5-square.js');
 class Square extends mySquare {
-  constructor (size) {
-    super(size, size);
-    this.size = size;
-  }
-  charPrint (c) {
-    if (c === undefined) {
-      c = 'X';
-    }
+  charPrint (c = 'X') {
     for (let s = 0; s < this.size; s++) {
       console.log(c.repeat(this.size));
     }
