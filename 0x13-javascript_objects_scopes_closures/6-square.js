6f05f681bd04c36e326df26a8e342315cf1e13f3
jozsa <allisonjoyweiner@gmail.com>
2019-05-13 22:18:46 +0000

    Formatting - added case for if c is undefined

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,6 +2,9 @@
 const mySquare = require('./5-square.js');
 class Square extends mySquare {
   charPrint (c = 'X') {
+    if (c === undefined) {
+      c = 'X';
+    }
     for (let s = 0; s < this.size; s++) {
       console.log(c.repeat(this.size));
     }
