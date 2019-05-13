2ba38238f1a98a27ef90139875ea78d60473b504
jozsa <allisonjoyweiner@gmail.com>
2019-05-13 22:58:50 +0000

    Return the number of occurences in a list

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
 const mySquare = require('./5-square.js');
 class Square extends mySquare {
-  charPrint (c = 'X') {
+  charPrint (c) {
+    if (c === undefined) {
+	    c = 'X'
+    }
     for (let s = 0; s < this.size; s++) {
       console.log(c.repeat(this.size));
     }
