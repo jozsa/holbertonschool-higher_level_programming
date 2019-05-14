368ac53914904eef50c4361eb3346ee0b13dfd51
jozsa <allisonjoyweiner@gmail.com>
2019-05-14 22:59:26 +0000

    Making URL a command line argument instead of hardcoded

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'http://swapi.co/api/films/';
+let url = process.argv[2];
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
