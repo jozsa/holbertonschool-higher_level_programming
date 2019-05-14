1e4c3ba9fa62d83be656068188dc6bb7b4d58e6c
jozsa <allisonjoyweiner@gmail.com>
2019-05-14 22:57:32 +0000

    Getting the count of Star Wars films Wedge Antilles was in

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+let url = 'http://swapi.co/api/films/';
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    let info = JSON.parse(body).results;
+    let count = 0;
+    info.forEach(function (element) {
+      if (element.characters.includes('https://swapi.co/api/people/18/')) { count++; }
+    });
+    console.log(count);
+  }
+});
