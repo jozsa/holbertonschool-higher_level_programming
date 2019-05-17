472aa5894ab05dc93dc81199e860866eaca35ccc
jozsa <allisonjoyweiner@gmail.com>
2019-05-16 18:35:14 -0700

    API request from star wars API and list all star wars movie titles in a ul/li tag

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+$(function () {
+    $.ajax({
+        type: 'GET',
+        url: 'https://swapi.co/api/films/?format=json',
+        success: function(films) {
+            $.each(films.results, function(i, film) {
+                    $("#list_movies").append("<li>"+ film.title +"</li>");
+            });
+        }
+    });
+});
