76bd2d651da2766c6a789487ab1d97d6fe52db4a
jozsa <allisonjoyweiner@gmail.com>
2019-05-16 19:00:03 -0700

    Display 'Hello' in another language based on API call

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+$(function() {
+    $.ajax({
+        type: 'GET',
+        url: 'https://fourtonfish.com/hellosalut/?lang=fr',
+        success: function(language) {
+            $("DIV#hello").text(language.hello);
+        }
+    });
+});
